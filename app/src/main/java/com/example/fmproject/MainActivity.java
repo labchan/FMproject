31599dcf1e2889ba6b4a56820b7da10693c8a873
dickey <dickey_lhchan@hotmail.com>
2019-04-22 13:35:55 +0800

    create database, table and insert data

--- app/src/main/java/com/example/fmproject/MainActivity.java
+++ app/src/main/java/com/example/fmproject/MainActivity.java
@@ -2,12 +2,52 @@ package com.example.fmproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SQLiteHelper myDb;
+    EditText editsite, editlocation, editcontact, edittel, editdescription;
+    Button editbtnAdd, editbtnList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new SQLiteHelper(this);
+
+        editsite=(EditText)findViewById(R.id.site);
+        editlocation=(EditText)findViewById(R.id.location);
+        editcontact=(EditText)findViewById(R.id.contact);
+        edittel=(EditText)findViewById(R.id.tel);
+        editdescription=(EditText)findViewById(R.id.description);
+        editbtnAdd=(Button) findViewById(R.id.btnAdd);
+        editbtnList=(Button) findViewById(R.id.btnList);
+        AddData();
+    }
+
+    public void AddData(){
+        editbtnAdd.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                            boolean isInserted= myDb.insertData(editlocation.getText().toString(),
+                                    editsite.getText().toString(),
+                                    editcontact.getText().toString(),
+                                    edittel.getText().toString(),
+                                    editdescription.getText().toString() ) ;
+                            if (isInserted =true)
+                                Toast.makeText(MainActivity.this, "Data Inserted", Toast.LENGTH_LONG).show();
+                            else
+                                 Toast.makeText(MainActivity.this, "Data not Inserted", Toast.LENGTH_LONG).show();
+
+
+                    }
+                }
+        );
     }
 }
--- app/src/main/java/com/example/fmproject/SQLiteHelper.java
+++ app/src/main/java/com/example/fmproject/SQLiteHelper.java
@@ -1,5 +1,6 @@
 package com.example.fmproject;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -13,10 +14,54 @@ import java.sql.Statement;
  */
 
 public class SQLiteHelper extends SQLiteOpenHelper{
-    public SQLiteHelper( Context context,  String name,  SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+
+    public static final String Database_Name="FM.db";
+    public static final String Table_Name="repairRecord";
+    public static final String col_1="id";
+    public static final String col_2="location";
+    public static final String col_3="site";
+    public static final String col_4="contact";
+    public static final String col_5="tel";
+    public static final String col_6="description";
+
+    // constructor
+    public SQLiteHelper(Context context) {
+        super(context, Database_Name, null, 1);
+
     }
 
+
+
+    // create table
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase){
+        sqLiteDatabase.execSQL(" create table "+Table_Name+ "(id INTEGER PRIMARY KEY AUTOINCREMENT, location TEXT, site TEXT, contact TEXT, tel TEXT, description TEXT)");
+    }
+
+    // checking table to be create if not create it
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
+        sqLiteDatabase.execSQL("drop table if exists " +Table_Name);
+        onCreate(sqLiteDatabase);
+    }
+
+    public boolean insertData(String location, String site, String contact, String tel, String description){
+        SQLiteDatabase sqLiteDatabase = this.getWritableDatabase();
+        ContentValues contenValues=new ContentValues();
+        contenValues.put(col_2, location);
+        contenValues.put(col_3, site);
+        contenValues.put(col_4, contact);
+        contenValues.put(col_5, tel);
+        contenValues.put(col_6, description);
+
+        // checking any record be insert
+        long result = sqLiteDatabase.insert(Table_Name, null, contenValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+    /*
     public void queryData (String sql){
         SQLiteDatabase database=getWritableDatabase();
         database.execSQL(sql);
@@ -34,20 +79,8 @@ public class SQLiteHelper extends SQLiteOpenHelper{
         statement.bindString(4, tel);
         statement.bindString(5 , description);
         statement.bindBlob(6, image);
+    } */
 
-
-
-
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase){
-
-    }
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1){
-
-    }
 }
 
 
